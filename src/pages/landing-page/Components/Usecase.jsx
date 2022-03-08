2f3144097d395ee91b0127c3487c7e52adabc3d5
PetersGlory <peterglo2200@gmail.com>
2022-03-08 04:46:37 -0800

    new update on usecase

--- src/pages/landing-page/Components/Usecase.jsx
+++ src/pages/landing-page/Components/Usecase.jsx
@@ -13,14 +13,14 @@ const Usecase = () => {
               </div>
               <div className="container">
                 <div className="row mt-5 justify-content-between">
-                  <div className="col-lg-3 col-sm-12 usecase p-5">
+                  <div className="col-lg-3 col-sm-12 usecase card p-5">
                     <h3 className="text-white"><b>DEX</b></h3>
                     <p style={{color:'#c9c9c9', fontWeight:'700'}}>
                       our decentralized exchange platform allow users to swap, stake , 
                       farm and add liquidity of any token on bsc
                     </p>
                   </div>
-                  <div className="col-lg-3 col-sm-12 usecase p-5">
+                  <div className="col-lg-3 col-sm-12 usecase card p-5">
                     <h3 className="text-white"><b>NFT MARKETPLACE</b></h3>
                     <p style={{color:'#c9c9c9', fontWeight:'700'}}>
                     our multichain nft marketplace allow creator to Create and list their 
@@ -28,14 +28,14 @@ const Usecase = () => {
                     usdt , bnb or other blockchain native coins
                     </p>
                   </div>
-                  <div className="col-lg-3 col-sm-12 usecase p-5">
+                  <div className="col-lg-3 col-sm-12 usecase card p-5">
                     <h3 className="text-white"><b>GAMIFICATION</b></h3>
                     <p style={{color:'#c9c9c9', fontWeight:'700'}}>
                     our play to earn gamification features will allow users to play our game and 
                     earn a portion of our token this features open to only users that stake their token…
                     </p>
                   </div>
-                  <div className="col-lg-3 col-sm-12 usecase p-5">
+                  <div className="col-lg-3 col-sm-12 usecase card p-5">
                     <h3 className="text-white"><b>LAUNCHPAD</b></h3>
                     <p style={{color:'#c9c9c9', fontWeight:'700'}}>
                     our multichain launchpad allow users to participate tokens 
@@ -44,28 +44,28 @@ const Usecase = () => {
                   </div>
 
 
-                  <div className="col-lg-3 mt-3 col-sm-12 usecase p-5">
+                  <div className="col-lg-3 mt-3 col-sm-12 usecase card p-5">
                     <h3 className="text-white"><b>WALLET</b></h3>
                     <p style={{color:'#c9c9c9', fontWeight:'700'}}>
                     Our multichain crypto wallet allow users to store, send and 
                     receive crypto currency of any blockchain securely.
                     </p>
                   </div>
-                  <div className="col-lg-3 mt-3 col-sm-12 usecase p-5">
+                  <div className="col-lg-3 mt-3 col-sm-12 usecase card p-5">
                     <h3 className="text-white"><b>WEB3 SOCIAL PLATFORM</b></h3>
                     <p style={{color:'#c9c9c9', fontWeight:'700'}}>
                     This allow users and crypto lovers to come together , 
                     chat, share post and watch videos on our social platform
                     </p>
                   </div>
-                  <div className="col-lg-3 mt-3 col-sm-12 usecase p-5">
+                  <div className="col-lg-3 mt-3 col-sm-12 usecase card p-5">
                     <h3 className="text-white"><b>MEDIA PLATFORM</b></h3>
                     <p style={{color:'#c9c9c9', fontWeight:'700'}}>
                     our media platform teaches users about crypto , defi and 
                     bring crypto near to people through our daily crypto tips , and news
                     </p>
                   </div>
-                  <div className="col-lg-3 mt-3 col-sm-12 usecase p-5">
+                  <div className="col-lg-3 mt-3 col-sm-12 usecase card p-5">
                     <h3 className="text-white"><b>CHARITY PLATFORM</b></h3>
                     <p style={{color:'#c9c9c9', fontWeight:'700'}}>
                     our charity platform helps less people in needs , 
